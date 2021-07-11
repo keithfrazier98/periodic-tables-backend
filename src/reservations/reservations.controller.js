745c67cd17cfa6138deba111f4708759e637764a
Keith Frazier <keithers98@gmail.com>
2021-07-11 13:14:05 -0700

    added raw function to console.log

--- src/reservations/reservations.controller.js
+++ src/reservations/reservations.controller.js
@@ -205,7 +205,7 @@ function validateReservation(req, res, next) {
   
       const today = new Date(Date.now());
       const offset = today.getTimezoneOffset() / 60
-      console.log("offset", offset)
+      console.log("offset", offset, today.getTimezoneOffset())
       const isATuesday = date.getDay() === 2;
       const compareYear = date.getFullYear() - today.getFullYear();
       const compareMonth = date.getMonth() - today.getMonth();
